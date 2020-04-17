c00390871846e2548145f978818aa1999c3ff241
ritwik0302 <ritshendre@gmail.com>
2020-04-17 19:46:09 +0300

    wedidit

--- sketch.js
+++ sketch.js
@@ -10,8 +10,10 @@ var bird, slingshot;
 
 var gameState = "onSling";
 
+var score = 0;
+
 function preload() {
-    backgroundImg = loadImage("sprites/bg.png");
+getBackgroundimage();
 }
 
 function setup(){
@@ -45,18 +47,25 @@ function setup(){
 }
 
 function draw(){
+    if (backgroundImg)
     background(backgroundImg);
     Engine.update(engine);
+    noStroke();
+    textSize(35);
+    fill(255);
+    text("score:"+score,900,50);
     //strokeWeight(4);
     box1.display();
     box2.display();
     ground.display();
     pig1.display();
+    pig1.score();
     log1.display();
 
     box3.display();
     box4.display();
     pig3.display();
+    pig3.score();
     log3.display();
 
     box5.display();
@@ -85,4 +94,19 @@ function keyPressed(){
     if(keyCode === 32){
        // slingshot.attach(bird.body);
     }
+}
+
+async function getBackgroundimage(){
+    var response = await fetch ("http://worldtimeapi.org/api/timezone/Asia/kolkata");
+    var responsejson = await response.json();
+    var datetime = responsejson.datetime;
+    var hour= datetime.slice(11,13);
+    if (hour>=06 && hour<=18 ){
+        bg= "sprites/bg.png"
+    }
+
+    else {
+        bg= "sprites/bg2.jpg"
+    }
+    backgroundImg= loadImage (bg);
 }
